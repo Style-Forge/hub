bf17e951623f427644c5c64102e9faa19dbcf3d8
Sarmaged <sarmaged@gmail.com>
2024-06-18 00:55:18 +0300

    Fixed .gitignore and Added config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Style-Forge",
+  description: "Style-Forge is a comprehensive CSS framework designed for modern web development. It offers customizable themes, utility classes, and support for flexbox and grid, enabling developers to create responsive and aesthetically pleasing websites efficiently",
+  base: '/style-forge/'
+})
