66e70cb196f0bbff73fc79c6dfaeedc8afd7be10
Sarmaged <sarmaged@gmail.com>
2024-06-18 00:44:10 +0300

    Set up documentation

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, frontmatter } = useData()
+</script>
+
+<template>
+  <div v-if="frontmatter.home">
+    <h1>{{ site.title }}</h1>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
