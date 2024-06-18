79f7c5204af1d7dea9db50ad5e4409d505107606
Sarmaged <sarmaged@gmail.com>
2024-06-18 09:51:49 +0300

    Fixed base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,8 +4,6 @@ export default defineConfig({
   title: "Style-Forge",
   description: "Style-Forge is a comprehensive CSS framework designed for modern web development. It offers customizable themes, utility classes, and support for flexbox and grid, enabling developers to create responsive and aesthetically pleasing websites efficiently",
 
-  base: '/style-forge/',
-
   themeConfig: {
     search: {
       provider: 'local'
