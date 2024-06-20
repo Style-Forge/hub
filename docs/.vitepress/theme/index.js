b179f0f04c798a2b74514d38186d160333679b9a
Sarmaged <sarmaged@gmail.com>
2024-06-20 18:17:34 +0300

    Fixed vitepress theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import './var.css'
 import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
-  extends: DefaultTheme,
+  ...DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {})
   },
